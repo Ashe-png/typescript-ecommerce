1f25a429b322476c7e36d1a75464741a3b3df46b
Ashe-png <ashish.khatakho11@gmail.com>
2023-07-08 21:04:21 +0545

    routes for auth

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -2,13 +2,14 @@ import express from 'express';
 
 const router = express.Router();
 
-// import {authCheck, adminCheck } from './middlewares/auth'
+//middlewares
+import { authCheck } from '../middlewares/auth';
 
 //controller
 
 import { createOrUpdateUser } from '../controllers/auth';
 
-router.post('/create-or-update-user', createOrUpdateUser);
+router.post('/create-or-update-user', authCheck, createOrUpdateUser);
 // router.post('/current-user', authCheck, currentUser);
 // router.post('/current-admin', authCheck, adminCheck, currentUser);
 
