47b235c5174eea018b975278e8bb1d9276b01b9e
Ashe-png <ashish.khatakho11@gmail.com>
2023-07-08 21:03:20 +0545

    created controllers

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -1,24 +1,27 @@
 import express, { Response, Request } from 'express';
+import User, { IUser } from '../models/user';
 
 type NewType = Request;
 
 export const createOrUpdateUser = async (req: Request, res: Response) => {
-  //   const { name, picture, email } = req.user;
-  //   const user = await User.findOneAndUpdate(
-  //     { email },
-  //     { name: email.split('@')[0], picture },
-  //     { new: true }
-  //   );
-  //   if (user) {
-  //     console.log('User Updated', user);
-  //     res.json(user);
-  //   } else {
-  //     const newUser = await new User({
-  //       email,
-  //       name: email.split('@')[0],
-  //       picture,
-  //     }).save();
-  //     console.log('User created', newUser);
-  //     res.json(newUser);
-  //   }
+  const { name, picture, email } = req.body.user;
+  console.log(req.body.user);
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name: email.split('@')[0], picture },
+
+    { new: true }
+  );
+  if (user) {
+    console.log('User Updated', user);
+    res.json(user);
+  } else {
+    const newUser = await new User({
+      email,
+      name: email.split('@')[0],
+      picture,
+    }).save();
+    console.log('User created', newUser);
+    res.json(newUser);
+  }
 };
