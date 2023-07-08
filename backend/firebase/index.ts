bf3515a9b470e4de428e91f7e8c4305c1faa72d4
Ashe-png <ashish.khatakho11@gmail.com>
2023-07-08 21:03:44 +0545

    firebase in typescript

--- backend/firebase/index.ts
+++ backend/firebase/index.ts
@@ -1,9 +1,8 @@
-var admin = require('firebase-admin');
-
-var serviceAccount = require('../config/fbServiceAccountKey.json');
+import * as admin from 'firebase-admin';
+import serviceAccount from '../config/fbServiceAcccountKey.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   databaseURL: 'https://ecom-4b6a0-default-rtdb.firebaseio.com',
 });
 
