3a93aab87dffc644f46927071a4d8d12d45fffd0
Ashe-png <ashish.khatakho11@gmail.com>
2023-07-09 19:23:30 +0545

    types for user and userState

--- frontend/src/reducers/userReducer.tsx
+++ frontend/src/reducers/userReducer.tsx
@@ -1,6 +1,21 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 
-export const userReducer = (state = null, action: PayloadAction<object>) => {
+type userT = {
+  name: string | null;
+  email: string | null;
+  token: string | null;
+  role: string | null;
+  _id: string | null;
+};
+
+export type userState = {
+  user: userT;
+} | null;
+
+export const userReducer = (
+  state: userState = null,
+  action: PayloadAction<object>
+) => {
   switch (action.type) {
     case 'LOGGED_IN_USER':
       return action.payload;
